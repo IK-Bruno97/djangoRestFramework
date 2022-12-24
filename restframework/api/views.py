b8ce329a3c7f02d0556220c30c014c8899e3f2b1
IK-Bruno97 <69594989+IK-Bruno97@users.noreply.github.com>
2022-12-24 16:48:23 +0100

    Add files via upload

---
+++ restframework/api/views.py
@@ -0,0 +1,106 @@
+import json
+from django.conf import settings
+import redis
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from . utils import *
+
+# Connect to our Redis instance
+redis_instance = redis.StrictRedis(host=settings.REDIS_HOST,
+                                  port=settings.REDIS_PORT, db=0)
+
+# Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+  thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+  }
+  return Response(thisdict)
+
+@api_view(['GET', 'POST'])
+def getNotes(request, *args, **kwargs):
+   if request.method == 'GET':
+      items = {}
+      count = 0
+      for key in redis_instance.keys("*"):
+            items[key.decode("utf-8")] = redis_instance.get(key)
+            count += 1
+      response = {
+            'count': count,
+            'msg': f"Found {count} items.",
+            'items': items
+      }
+      return Response(response, status=200)
+    
+   if request.method == 'POST':
+      item = json.loads(request.body)
+      key = list(item.keys())[0]
+      value = item[key]
+      redis_instance.set(key, value)
+      response = {
+            'msg': f"{key} successfully set to {value}"
+      }
+      return Response(response, 201)
+
+  
+@api_view(['GET', 'PUT', 'DELETE'])
+def getNote(request, *args, **kwargs):
+    if request.method == 'GET':
+      if request.method == 'GET':
+        if kwargs['key']:
+          value = redis_instance.get(kwargs['key'])
+          if value:
+              response = {
+                    'key': kwargs['key'],
+                    'value': value,
+                    'msg': 'success'
+              }
+              return Response(response, status=200)
+          else:
+              response = {
+                  'key': kwargs['key'],
+                   'value': None,
+                  'msg': 'Not found'
+              }
+              return Response(response, status=404)
+
+    if request.method == 'PUT':
+        if kwargs['key']:
+            request_data = json.loads(request.body)
+            new_value = request_data['new_value']
+            value = redis_instance.get(kwargs['key'])
+            if value:
+                redis_instance.set(kwargs['key'], new_value)
+                response = {
+                    'key': kwargs['key'],
+                    'value': value,
+                    'msg': f"Successfully updated {kwargs['key']}"
+                }
+                return Response(response, status=200)
+            else:
+                response = {
+                    'key': kwargs['key'],
+                    'value': None,
+                    'msg': 'Not found'
+                }
+                return Response(response, status=404)
+
+    if request.method == 'DELETE':
+        if kwargs['key']:
+            result = redis_instance.delete(kwargs['key'])
+            if result == 1:
+                response = {
+                    'msg': f"{kwargs['key']} successfully deleted"
+                }
+                return Response(response, status=404)
+            else:
+                response = {
+                    'key': kwargs['key'],
+                    'value': None,
+                    'msg': 'Not found'
+                }
+                return Response(response, status=404)
